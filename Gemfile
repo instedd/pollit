4543efc5ea346bd476c11fa723cfa9c35da9a788
Santiago Palladino <spalladino@users.noreply.github.com>
2011-08-10 16:02:43 -0300

    Added spork for running tests

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc5'
+gem 'mysql2', '0.3.6'
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'compass'
+gem 'devise'
+gem 'therubyracer'
+gem "arel", "2.1.4"
+gem "enumerated_attribute"
+gem 'nokogiri'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "~> 3.1.0.rc"
+  gem 'coffee-rails', "~> 3.1.0.rc"
+  gem 'uglifier'
+end
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+  gem "rspec-rails", "~> 2.6"
+  gem 'machinist', '~> 1.0.6'
+  gem 'faker'
+  gem 'mocha'
+  gem 'webmock'
+  gem 'ci_reporter'
+  gem 'spork', '~> 0.9.0'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,36 @@
+require 'spork'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+
+  require File.expand_path("../../config/environment", __FILE__)
+  require File.expand_path(File.dirname(__FILE__) + '/blueprints')
+  require 'rspec/rails'
+  require 'webmock/rspec'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  # Configure rspec
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.use_transactional_fixtures = true
+    config.render_views = true
+
+    config.before(:each) do 
+      Sham.reset
+    end    
+  end
+
+  # Configure webmock
+  WebMock.allow_net_connect!
+  class WebMock::RequestStub
+    def to_return_file(file)
+      self.to_return(:body => File.new("#{Rails.root}/spec/webmocks/#{file}"), :status => 200)
+    end
+  end
+
+end
+
+Spork.each_run do
+  # Code to be executed each time the specs are run  
+end

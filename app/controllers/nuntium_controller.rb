9bd5de9a1cd3c46c7343275c64d900806094a95f
Santiago Palladino <spalladino@users.noreply.github.com>
2011-08-10 16:03:57 -0300

    Nuntium controller created

---
+++ app/controllers/nuntium_controller.rb
@@ -0,0 +1,12 @@
+class NuntiumController < ApplicationController
+
+  def receive_at
+    logger.debug "Received nuntium message: #{params.inspect}"
+    poll = find_poll_by_channel
+    user = find_user_by_from_sms
+    send_response_from_user_to_poll
+    reply_with_next_question_to_user
+    render :nothing => true
+  end
+
+end

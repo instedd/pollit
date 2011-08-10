78530e72c83716771e43808b7f79e0d059d1683e
Matias Hick <unformatt@gmail.com>
2011-08-10 15:29:38 -0300

    authentication with google

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110809174011) do
+ActiveRecord::Schema.define(:version => 20110810161216) do
 
   create_table "polls", :force => true do |t|
     t.string   "title"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(:version => 20110809174011) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "google_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

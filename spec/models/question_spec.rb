b81331312459874c2d1af256ae8047db17a1a8f5
Santiago Palladino <spalladino@users.noreply.github.com>
2011-08-12 16:10:09 -0300

    Question to message

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -18,4 +18,21 @@ describe Question do
   it "belongs to a poll" do
     Question.make.poll.should_not be_nil
   end
+
+  context "as message" do
+    it "can be set to message being text" do
+      Question.make(:text, :title => "A question?").to_message.should\
+        eq("A question?")
+    end
+
+    it "can be set to message being options" do
+      Question.make(:options, :title => "An options question?", :options => %w(foo bar baz)).to_message.should\
+        eq("An options question? a-foo b-bar c-baz")
+    end
+
+    it "can be set to message being numeric" do
+      Question.make(:numeric, :title => "A numeric question?", :numeric_min => 1, :numeric_max => 4).to_message.should\
+        eq("A numeric question? 1-4")
+    end
+  end
 end

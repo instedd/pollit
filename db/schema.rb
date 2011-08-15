cd42071ae2ef786e4d419d00f75f0353b84a8d3c
Matias Hick <unformatt@gmail.com>
2011-08-15 10:48:57 -0300

    creating respondent model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110812182944) do
+ActiveRecord::Schema.define(:version => 20110812191541) do
 
   create_table "polls", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20110812182944) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "channel"
   end
 
   create_table "questions", :force => true do |t|
@@ -31,6 +32,13 @@ ActiveRecord::Schema.define(:version => 20110812182944) do
     t.integer  "poll_id"
     t.integer  "numeric_max"
     t.integer  "numeric_min"
+  end
+
+  create_table "respondents", :force => true do |t|
+    t.string   "phone"
+    t.integer  "poll_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|

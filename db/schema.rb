6cc39a7436b307d0fbd57ccf58ad38f8e09445a8
Matias Hick <unformatt@gmail.com>
2011-08-15 14:44:03 -0300

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110812191541) do
+ActiveRecord::Schema.define(:version => 20110815152205) do
+
+  create_table "answers", :force => true do |t|
+    t.integer  "poll_id"
+    t.integer  "respondent_id"
+    t.string   "response"
+    t.integer  "response_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "polls", :force => true do |t|
     t.string   "title"

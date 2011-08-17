6a98e4966c918badc163d9652cf0c9f43e41e70c
Matias Hick <unformatt@gmail.com>
2011-08-17 10:43:31 -0300

    starting poll

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,7 +2,7 @@ class Poll < ActiveRecord::Base
   belongs_to :owner, :class_name => User.name
   
   has_many :questions
-  has_many :respondants
+  has_many :respondents
 
   validate :title, :presence => true, :length => {:maximum => 64}
 
@@ -13,13 +13,18 @@ class Poll < ActiveRecord::Base
     app_password = Pollit::Application.config.nuntium_app_password
 
     api = Nuntium.new service_url, account_name, app_name, app_password
-    
-    message = {
-      :from => "sms://0",
-      :to => "sms://5678",
-      :body => "Hello Nuntium!"
-    }
-    
+    messages = []
+
+    respondents.each do |respondent|
+      messages << {
+        :from => Pollit::Application.config.nuntium_message_from,
+        :to => respondent.phone,
+        :body => "Hello Nuntium!"
+      }
+    end
+
+    api.send_ao messages
+    self.status = :started
   end
 
   include Parser

8adb678d51b11806bba5e50dd097e1a22af6f729
Matias Hick <unformatt@gmail.com>
2011-08-23 10:19:58 -0300

    nuntium content type

--- app/controllers/nuntium_controller.rb
+++ app/controllers/nuntium_controller.rb
@@ -1,16 +1,19 @@
 class NuntiumController < ApplicationController
-
   def receive_at
     logger.debug "Received nuntium message: #{params.inspect}"
     poll = Poll.find_by_channel(params[:channel])
     respondent = Respondent.find_by_phone(params[:from])
-    next_message = poll.accept_answer(params[:body], respondent)
 
-    if next_message.nil?
+    if (respondent.nil? || poll.nil?)
       render :nothing => true
     else
-      render :text => next_message
+      next_message = poll.accept_answer(params[:body], respondent)
+
+      if next_message.nil?
+        render :nothing => true
+      else
+        render :content_type => "text/plain", :text => next_message
+      end
     end
   end
-
 end

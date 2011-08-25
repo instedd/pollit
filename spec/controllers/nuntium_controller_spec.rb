2d26ad8cab55d3e92d5450de876e5fe6bad5c449
Matias Hick <unformatt@gmail.com>
2011-08-25 16:10:24 -0300

    tests added

--- spec/controllers/nuntium_controller_spec.rb
+++ spec/controllers/nuntium_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe NuntiumController do
+  before_each_sign_in_as_new_user
 
+  it "should not receive answer without correct" do
+    post :receive_at
+    @response.body.should be_blank
+  end
+
+  it "should receive answer" do
+    p = Poll.make :with_questions, :channel => "test"
+
+    post :receive_at, :channel => "test", :from => p.respondents.first.phone, :body => "yes"
+
+    p.respondents.first.confirmed.should be_true
+  end
 end

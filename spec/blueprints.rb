f78294050153dd78ea98521e6377c04e032d9b1c
Matias Hick <unformatt@gmail.com>
2011-08-25 16:10:01 -0300

    blueprints updated

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -32,14 +32,77 @@ end
 Poll.blueprint(:with_questions) do
   title
   description
+  confirmation_word {"yes"}
   form_url      {Sham.url}
   post_url      {Sham.url}
   owner         {User.make}
   questions     {[
-    Question.make(:field_name => 'entry.0.single'),
-    Question.make(:options, :options => %w(foo bar baz), :field_name => 'entry.1.single'),
-    Question.make(:options, :options => %w(oof rab zab), :field_name => 'entry.2.group'),
-    Question.make(:numeric, :numeric_min => 1, :numeric_max => 10, :field_name => 'entry.3.group')]}
+    Question.make(:field_name => 'entry.0.single', :position => 1),
+    Question.make(:options, :options => %w(foo bar baz), :field_name => 'entry.1.single', :position => 2),
+    Question.make(:options, :options => %w(oof rab zab), :field_name => 'entry.2.group', :position => 3),
+    Question.make(:numeric, :numeric_min => 1, :numeric_max => 10, :field_name => 'entry.3.group', :position => 4)]}
+  respondents   {[
+    Respondent.make(:phone => "1111"),
+    Respondent.make(:phone => "2222"),
+    Respondent.make(:phone => "3333"),
+    Respondent.make(:phone => "4444"),
+    Respondent.make(:phone => "5555")
+  ]}
+end
+
+Poll.blueprint(:with_text_questions) do
+  title
+  description
+  confirmation_word {"yes"}
+  form_url      {Sham.url}
+  post_url      {Sham.url}
+  owner         {User.make}
+  questions     {[
+    Question.make(:field_name => 'entry.0.single', :position => 1),
+    Question.make(:field_name => 'entry.1.single', :position => 2),
+    Question.make(:field_name => 'entry.2.single', :position => 3)]}
+  respondents   {[
+    Respondent.make(:phone => "sms://1111"),
+    Respondent.make(:phone => "sms://2222"),
+    Respondent.make(:phone => "sms://3333"),
+    Respondent.make(:phone => "sms://4444"),
+    Respondent.make(:phone => "sms://5555")]}
+end
+
+Poll.blueprint(:with_option_questions) do
+  title
+  description
+  confirmation_word {"yes"}
+  form_url      {Sham.url}
+  post_url      {Sham.url}
+  owner         {User.make}
+  questions     {[
+    Question.make(:options, :options => %w(foo bar baz), :field_name => 'entry.1.single', :position => 1),
+    Question.make(:options, :options => %w(oof rab zab), :field_name => 'entry.2.single', :position => 2)]}
+  respondents   {[
+    Respondent.make(:phone => "1111"),
+    Respondent.make(:phone => "2222"),
+    Respondent.make(:phone => "3333"),
+    Respondent.make(:phone => "4444"),
+    Respondent.make(:phone => "5555")]}
+end
+
+Poll.blueprint(:with_numeric_questions) do
+  title
+  description
+  confirmation_word {"yes"}
+  form_url      {Sham.url}
+  post_url      {Sham.url}
+  owner         {User.make}
+  questions     {[
+    Question.make(:numeric, :numeric_min => 1, :numeric_max => 10, :field_name => 'entry.0.group', :position => 1),
+    Question.make(:numeric, :numeric_min => 30, :numeric_max => 40, :field_name => 'entry.1.group', :position => 2)]}
+  respondents   {[
+    Respondent.make(:phone => "1111"),
+    Respondent.make(:phone => "2222"),
+    Respondent.make(:phone => "3333"),
+    Respondent.make(:phone => "4444"),
+    Respondent.make(:phone => "5555")]}
 end
 
 Question.blueprint do
@@ -67,6 +130,7 @@ end
 Question.blueprint(:numeric) do
   title
   description
+  position      {1}
   kind          {:numeric}
   poll          {Poll.make}
   numeric_min   {rand(3)+1}

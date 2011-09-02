32614b9ba01f896c3cec34467701bc8aed4190f0
Matias Hick <unformatt@gmail.com>
2011-09-02 11:48:46 -0300

    changed rescue_from implemenatation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from Exceptions::InvalidAction do
-      render :file => "public/400.html", :status => :badrequest, :layout => nil
-    end
-  end
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
   private
   
+  def record_not_found
+    render :file => "public/400.html", :layout => nil
+  end
+
   def after_sign_in_path_for(resource_or_scope)
     (session[:return_to] || polls_path).to_s
   end
-
 end

b75dfcb94aad30ebec9a1682bf0edc14da4342d3
Matias Hick <unformatt@gmail.com>
2011-09-12 12:49:04 -0300

    import from csv in the same place with manual phone creation

--- app/controllers/respondents_controller.rb
+++ app/controllers/respondents_controller.rb
@@ -10,45 +10,39 @@ class RespondentsController < ApplicationController
 
   def batch_update
     return if @poll.started?
-    Respondent.delete_all :poll_id => @poll.id
 
-    phones = params[:phones].map { |phone| "sms://#{phone}" }
-
-    phones.each do |phone|
-      unless @poll.respondents.find_by_phone(phone)
-        @poll.respondents.create(:phone => phone)
-      end
-    end
+    update_phone_list(params[:phones])
 
     head :ok
   end
 
-  def import_phones
-    
-  end
-
   def import_csv
-    return if @poll.started?
-    redirect_to @poll if @poll.started?
-    redirect_to @poll unless params[:csv].content_type == "text/csv"
-
-    Respondent.delete_all :poll_id => @poll.id
-
-    CSV.new(params[:csv].tempfile).each do |row|
-      phone = row.first.gsub(/[^0-9]/, "")
-
-      unless @poll.respondents.find_by_phone(phone)
-        @poll.respondents.create(:phone => phone)
-      end
+    if @poll.started?
+      redirect_to @poll, :notice => "poll already started"
     end
 
-    redirect_to @poll
+    unless params[:csv].content_type == "text/csv"
+      redirect_to @poll, :notice => "file should be in csv format"
+    end
+
+    phones = CSV.read(params[:csv].tempfile).map(&:first)
+    update_phone_list(phones)
+
+    redirect_to @poll, :notice => "csv phones has been imported"
   end
 
   private
 
+  def update_phone_list(phones)
+    Respondent.delete_all :poll_id => @poll.id
+
+    phones.each do |phone|
+      prefixed_phone = "sms://#{phone.gsub(/[^0-9]/,"")}"
+      @poll.respondents.create(:phone => prefixed_phone)
+    end
+  end
+
   def load_poll
     @poll = Poll.find params[:poll_id]
   end
-
 end

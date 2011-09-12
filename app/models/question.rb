51dee51bc91da29b957ce232b64826fe048e6bb6
Matias Hick <unformatt@gmail.com>
2011-09-12 12:52:24 -0300

    improved validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,20 +1,20 @@
 class Question < ActiveRecord::Base
-  acts_as_list :scope => :poll
-
   OptionsIndices = Array('a'..'z')
 
   belongs_to :poll
   has_many :answers
 
   validates :title,   :presence => true
+  validates :description, :presence => true
+  validates :field_name, :presence => true
+  validates :position, :presence => true
   validates :options, :presence => true, :if => :kind_options?
   validates :message, :length => {:maximum => 140}
-
   validates_numericality_of :numeric_min, :only_integer => true, :if => lambda{|q| q.kind_numeric? && q.numeric_min }
   validates_numericality_of :numeric_max, :only_integer => true, :if => lambda{|q| q.kind_numeric? && q.numeric_max }
 
+  acts_as_list :scope => :poll
   serialize :options, Array
-  
   enum_attr :kind, %w(^text options numeric)
 
   def message

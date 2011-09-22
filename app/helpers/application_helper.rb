0ee32b56f1e1beab44c0ce7e3ccdebc7e1778fa5
Matias Hick <unformatt@gmail.com>
2011-09-22 12:44:00 -0300

    helper for angular js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def angular_js_include_tag
+    raw(content_tag(:script, nil, {
+      "ng:autobind" => "", 
+      :src => "/assets/angular/angular-0.9.19.min.js"
+      }, escape=false))
+  end
 end

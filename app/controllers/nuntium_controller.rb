6fcb3e248e941b67ed28440264ae943eea1d1064
Matias Hick <unformatt@gmail.com>
2011-09-23 13:23:20 -0300

    bugfix AO with inexistent respondent

--- app/controllers/nuntium_controller.rb
+++ app/controllers/nuntium_controller.rb
@@ -2,19 +2,23 @@ class NuntiumController < ApplicationController
   def receive_at
     logger.debug "Received nuntium message: #{params.inspect}"
     
-    poll = Poll.includes(:channel).where("channels.name = ?", params[:channel]).first
-    respondent = Respondent.find_by_phone(params[:from])
+    begin
+      poll = Poll.includes(:channel).where("channels.name = ?", params[:channel]).first
+      respondent = poll.respondents.find_by_phone(params[:from])
 
-    if (respondent.nil? || poll.nil?)
-      render :nothing => true
-    else
-      next_message = poll.accept_answer(params[:body], respondent)
-
-      if next_message.nil?
+      if (respondent.nil? || poll.nil?)
         render :nothing => true
       else
-        render :content_type => "text/plain", :text => next_message
+        next_message = poll.accept_answer(params[:body], respondent)
+
+        if next_message.nil?
+          render :nothing => true
+        else
+          render :content_type => "text/plain", :text => next_message
+        end
       end
+    rescue Exception => e
+      render :nothing => true
     end
   end
 end

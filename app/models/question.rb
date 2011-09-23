69a4fd2ee2488d5b0671947435ffbfdfed36a32a
Matias Hick <unformatt@gmail.com>
2011-09-23 17:29:14 -0300

    help text is no longer required

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,7 +5,6 @@ class Question < ActiveRecord::Base
   has_many :answers
 
   validates :title, :presence => true
-  validates :description, :presence => true
   validates :field_name, :presence => true
   validates :position, :presence => true
   validates :options, :presence => true, :if => :kind_options?

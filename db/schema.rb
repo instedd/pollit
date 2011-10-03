e48e09addf693f50561fa9494bd686bf8fce995b
Matias Hick <unformatt@gmail.com>
2011-10-03 10:33:03 -0300

    added verification to user accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110913191741) do
+ActiveRecord::Schema.define(:version => 20110930182842) do
 
   create_table "answers", :force => true do |t|
     t.integer  "respondent_id"
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(:version => 20110913191741) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "welcome_message"
+    t.string   "status"
     t.string   "post_url"
     t.string   "confirmation_word"
-    t.string   "status"
     t.string   "goodbye_message"
   end
 
@@ -84,6 +84,9 @@ ActiveRecord::Schema.define(:version => 20110913191741) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "google_token"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

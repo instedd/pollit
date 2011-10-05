bb277505ebdac42e34370f45169670c4ffe44d3a
Santiago Palladino <spalladino@users.noreply.github.com>
2011-10-05 13:29:54 -0300

    Nuntium basic auth check

--- spec/controllers/nuntium_controller_spec.rb
+++ spec/controllers/nuntium_controller_spec.rb
@@ -3,21 +3,32 @@ require 'spec_helper'
 describe NuntiumController do
   before_each_sign_in_as_new_user
 
+  include AuthHelper
+
+  it "should fail auth" do
+    http_login 'wrong', 'auth'
+    post :receive_at
+    @response.status.should eq(401)
+  end
+
   it "should not receive answer without correct" do
+    nuntium_http_login
     post :receive_at
     @response.body.should be_blank
   end
 
-  it "should receive answer" do
-    p = Poll.make :with_questions, :owner => User.first
+  it "should receive confirmation" do
+    p = Poll.make :with_questions, :confirmation_word => "Yes"
     p.start
     
+    nuntium_http_login
     post :receive_at, {
-      :channel => "testing", 
+      :channel => p.channel.name, 
       :from => p.respondents.first.phone, 
-      :body => "yes"
+      :body => "Yes"
     }
 
-    #p.respondents.first.confirmed.should be_true
+    @response.body.should eq(p.questions.first.message)
+    p.reload.respondents.first.confirmed.should be_true
   end
 end

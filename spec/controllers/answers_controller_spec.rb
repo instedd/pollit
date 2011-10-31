ae48de292e75e6ec22189499c4a9a97c7b475b70
spalladino <spalladino@localhost>
2011-10-31 16:57:57 -0300

    Home page images

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,7 +4,7 @@ describe AnswersController do
 
   before_each_sign_in_as_new_user
 
-  describe "GET 'index'" do
+  describe "get index" do
     it "should be successful" do
       p = Poll.make :with_questions, :owner => controller.current_user
       get 'index', :poll_id => p.id

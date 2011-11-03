a123adbdd46c0e60cd80466af156c56325961339
Santiago Palladino <spalladino@users.noreply.github.com>
2011-11-03 15:50:26 -0300

    Minor fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Pollit::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'pollit-stg.heroku.com' }
-  ActionMailer::Base.default :from => "noreply@instedd.org"
+  ActionMailer::Base.default :from => "Pollit <noreply@instedd.org>"
 
   # Enable threaded mode
   # config.threadsafe!

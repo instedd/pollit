e1fd60d75565ca7fc7ce4f5a283b782b5f75ba52
spalladino <spalladino@localhost>
2011-11-17 18:24:50 -0300

    User language in db

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module Pollit
     config.default_locale = :en
 
     # Gettext configuration
-    FastGettext.add_text_domain 'app', :path => 'config/locales', :type => :po
+    FastGettext.add_text_domain 'app', :path => 'config/locales', :type => :po, :ignore_fuzzy => true, :ignore_obsolte => true
     FastGettext.default_available_locales = config.available_locales.keys.map(&:to_s)
     FastGettext.default_text_domain = 'app'
     FastGettext.default_locale = 'en'

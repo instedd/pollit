89691b0498954d1a18ee1d034d3d1b4f9506a58b
Adrian Romero <aromero@manas.com.ar>
2011-12-13 17:20:06 -0300

    Fixed test, cleaned code

--- db/migrate/20111213154341_set_default_value_for_respondent.rb
+++ db/migrate/20111213154341_set_default_value_for_respondent.rb
@@ -3,10 +3,6 @@ class SetDefaultValueForRespondent < ActiveRecord::Migration
     change_table :respondents do |t|
       t.change :current_question_sent, :boolean, :default => false, :null => false
       t.change :confirmed, :boolean, :default => false, :null => false
-      # t.change_default :current_question_sent, false
-      # t.change :current_question_sent, :null => false
-      # t.change_default :confirmed, false
-      # t.change :confirmed, :null => false
     end
   end
 
@@ -14,10 +10,6 @@ class SetDefaultValueForRespondent < ActiveRecord::Migration
     change_table :respondents do |t|
       t.change :current_question_sent, :boolean, :default => nil, :null => true
       t.change :confirmed, :boolean, :default => nil, :null => true
-      # t.change_default :current_question_sent, nil
-      # # t.change :current_question_sent, :null => true
-      # t.change_default :confirmed, nil
-      # # t.change :confirmed, :null => true
     end
   end
 end

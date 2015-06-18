58000cc6b1086c1f9b434ddb6fed3f7509233c7a
Santiago Palladino <spalladino@gmail.com>
2015-06-18 16:22:15 -0300

    #27 Moved respondents logic to separate js asset

---
+++ app/assets/javascripts/respondents.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+  $('#numberText').placeholder();
+  $(":input[type=text]").live('focus', function() {
+    $(this).numeric({ decimal: false, negative: false });
+  });
+});
+
+function PhonesCtrl() {
+  var scope = this;
+  scope.phones = gon.can_edit ? gon.phones : [];
+  scope.fixed_phones = gon.can_edit ? [] : gon.phones;
+  scope.only_add = !!gon.can_edit;
+
+  $(document).ready(function() {
+    window.setTimeout(function() {
+      $("#numberText").focus().blur();
+    }, 200);
+
+    new AjaxUpload($('#import_csv'), {
+      action: gon.import_csv_poll_respondents_path,
+      name: 'csv',
+      onSubmit: function(file, ext){
+        if(ext != 'csv') {
+          $.status.showError(file_should_be_in_csv_format, 6000)
+          return false;
+        }
+      },
+      onComplete: function(file, response){
+        var data = eval(response);
+        $.each(data, function(i, phone) {
+          if (!scope.phoneExists(phone.number)) {
+            scope.phones.push(phone);
+          }
+        });
+        scope.$eval();
+        $(".ng-directive #numberText").focus().blur();
+        $.status.showNotice(file_successfully_uploaded, 6000);
+      }
+    });
+  });
+
+  scope.phoneExists = function(phoneNumber) {
+    var all_phones = _.union(scope.fixed_phones, scope.phones);
+    return _.any(all_phones, function(phone) {
+      return phone.number == phoneNumber;
+    });
+  }
+
+  scope.addPhone = function() {
+    if ((!scope.phoneExists(scope.numberText)) && $.trim(scope.numberText) != '') {
+      scope.phones.push({number:scope.numberText});
+      scope.numberText = '';
+    }
+  };
+
+  scope.removePhone = function(phoneNumber) {
+    scope.phones = _.reject(scope.phones, function(phone) {
+      return phone.number == phoneNumber;
+    });
+    $(".ng-directive #numberText").focus().blur();
+  }
+
+  scope.removeEmptyPhones = function() {
+    scope.phones = _.reject(scope.phones, function(phone) {
+      return $.trim(phone.number) == '';
+    })
+  }
+
+  scope.saveChanges = function(showNotice, nextUrl) {
+    scope.removeEmptyPhones();
+    var phones = _.map(scope.phones, function(phone) { return phone.number });
+    $.post(gon.batch_update_poll_respondents_path, {'phones': phones}, function(data, textStatus) {
+      if (textStatus == "success") {
+        scope.onSaved();
+        if (showNotice) $.status.showNotice(phones_saved_successfully, 6000);
+        if (nextUrl) location.href = nextUrl;
+      } else {
+        $.status.showError(error_saving_phones, 6000);
+      }
+    });
+  };
+
+  scope.onSaved = function() {
+    if (scope.only_add) {
+      var current_phones = scope.phones;
+      scope.phones = [];
+      angular.forEach(current_phones, function(phone) {
+        scope.fixed_phones.push(phone);
+      });
+    }
+  }
+
+  scope.nextStep = function() {
+    scope.saveChanges(false, gon.poll_path);
+  }
+};
--- app/controllers/respondents_controller.rb
+++ app/controllers/respondents_controller.rb
@@ -1,17 +1,17 @@
 # Copyright (C) 2011-2012, InSTEDD
-# 
+#
 # This file is part of Pollit.
-# 
+#
 # Pollit is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Pollit is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Pollit.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -26,6 +26,12 @@ class RespondentsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:import_csv]
 
   def index
+    gon.phones = phones = @poll.respondents.map{|x| {:number => x.unprefixed_phone}}
+    gon.can_edit = @can_edit = @poll.status_configuring?
+    gon.import_csv_poll_respondents_path = import_csv_poll_respondents_path(@poll)
+    gon.batch_update_poll_respondents_path = batch_update_poll_respondents_path
+    gon.poll_path = poll_path(@poll, :wizard => true)
+
     if wizard?
       @wizard_step = _("Respondents")
       render :layout => 'wizard'

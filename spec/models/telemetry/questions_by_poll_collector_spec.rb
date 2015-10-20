56643f4e5a0631f2ed96b3dd8c1b45541f4d3e79
Adrian Romero <aromero@manas.com.ar>
2015-10-20 15:27:45 -0300

    Fixed spec in questions by poll collector

--- spec/models/telemetry/questions_by_poll_collector_spec.rb
+++ spec/models/telemetry/questions_by_poll_collector_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe Telemetry::QuestionsByPollCollector, telemetry: true do
 
   it 'counts questions by poll' do
-    poll_1 = Poll.make!
-    poll_2 = Poll.make!
+    q_1_1 = Question.make! created_at: to - 1.day
+    q_1_2 = Question.make! created_at: to - 10.days
+    q_1_3 = Question.make! created_at: to - 10.days
+    q_1_4 = Question.make! created_at: to - 12.days
 
-    Question.make! poll: poll_1, created_at: to - 1.day
-    Question.make! poll: poll_1, created_at: to - 10.days
-    Question.make! poll: poll_1, created_at: to - 10.days
-    Question.make! poll: poll_1, created_at: to - 12.days
+    q_2_1 = Question.make! created_at: to - 1.day
+    q_2_2 = Question.make! created_at: to - 30.days
+    q_2_3 = Question.make! created_at: to + 1.day
 
-    Question.make! poll: poll_2, created_at: to - 1.day
-    Question.make! poll: poll_2, created_at: to - 30.days
-    Question.make! poll: poll_2, created_at: to + 1.day
+    poll_1 = Poll.make! questions: [q_1_1, q_1_2, q_1_3, q_1_4]
+    poll_2 = Poll.make! questions: [q_2_1, q_2_2, q_2_3]
 
     stats = Telemetry::QuestionsByPollCollector.collect_stats period
     counters = stats[:counters]
